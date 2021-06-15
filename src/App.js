bbeac27a009223ad703aba7774040a73ec03c14a
viviane <v.buschwallace@yahoo.com>
2021-06-15 14:42:06 +0200

    added some router, switch, route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { Browserrouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="header"></header>
-    </div>
+    <Router className="App">
+      <Navigation />
+      <Switch>
+        <Route path={process.env.PUBLIC_URL + "/"} exact component={Home} />
+        <Route
+          path={process.env.PUBLIC_URL + "/About"}
+          exact
+          component={About}
+        />
+
+        <Route path={() => "/main" || "/anything"}>
+          <Home />
+        </Route>
+      </Switch>
+      {/* <header className="header"></header> */}
+    </Router>
   );
 }
 
