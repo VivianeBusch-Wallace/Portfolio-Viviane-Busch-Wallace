670ab26141f2f5d9d32700470bc9e4d220d2485f
viviane <v.buschwallace@yahoo.com>
2021-06-15 15:32:09 +0200

    added navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,22 @@
 import React from "react";
 const Navigation = () => {
-  return <div className="navigation-wrapper"></div>;
+  return (
+    <nav>
+      <ul className="navigation">
+        <li>
+          <link to="/">Home</link>
+        </li>
+        <li>
+          <link to="/about">About</link>
+        </li>
+        <li>
+          <link to="/projects">Projects</link>
+        </li>
+        <li>
+          <link to="/contact">Contact</link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 export default Navigation;
