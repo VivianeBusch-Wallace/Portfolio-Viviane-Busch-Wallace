3f420dc4294acdafa6877d50b84a3ac33b61c890
viviane <v.buschwallace@yahoo.com>
2021-06-16 10:35:59 +0200

    added notes in projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,3 +3,12 @@ const Projects = () => {
   return <div className="projects-wrapper">PROJECTS</div>;
 };
 export default Projects;
+
+// Notes during coding
+
+// ideas:
+
+// do not forget:
+
+// issues encountered:
+// how to make an infinite gallery?
