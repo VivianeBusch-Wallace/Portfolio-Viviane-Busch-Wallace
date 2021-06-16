fb16b561f50ed649c00b83c372d91ee661ac81b1
viviane <v.buschwallace@yahoo.com>
2021-06-16 11:24:25 +0200

    imported projects and destructed them in App, will change json file keys, added notes in App

--- src/App.js
+++ src/App.js
@@ -8,8 +8,24 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
+// import projects json (then map and send to projects component)
+import CodingProjects from "./projects.json";
+
 function App() {
   console.log(process.env.PUBLIC_URL);
+
+  const projectItem = CodingProjects.map((proj) => {
+    const {
+      id,
+      projectTitle,
+      shortDescription,
+      longDescription,
+      specialFeatures,
+      tools,
+      source,
+    } = proj;
+  });
+
   return (
     <Router>
       <Navigation />
@@ -31,3 +47,13 @@ function App() {
 }
 
 export default App;
+
+// Notes during coding
+
+// ideas:
+
+// do not forget:
+// when sedn props write route differently
+
+// issues encountered:
+// destructing projects with space in the key does not work > temp solution: make it one word
