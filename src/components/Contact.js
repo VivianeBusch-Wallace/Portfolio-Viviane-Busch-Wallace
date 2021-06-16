01c06b679a06e6186468d6e3a6b1330a8bd77d41
viviane <v.buschwallace@yahoo.com>
2021-06-16 09:01:53 +0200

    added html social media icons in contact, adding fontawesome to make them visible

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,12 +2,55 @@ import React from "react";
 const Contact = () => {
   return (
     <div className="contact-wrapper">
+      <div className="social-media">
+        <h3>Contact me!</h3>
+        <p>Have questions? Wanna chat? This is where you can find me:</p>
+        <div class="social-media icon-container">
+          <div class="icon">
+            <a href="https://github.com/VivianeBusch-Wallace">
+              <i class="fab fa-github-square"></i>
+            </a>
+          </div>
+          <div class="icon">
+            <a
+              href="https://www.linkedin.com/in/viviane-busch-wallace-68a117179/"
+              target="_blank"
+            >
+              <i class="fab fa-linkedin"></i>
+            </a>
+          </div>
+          <div class="icon">
+            <a
+              href="https://www.xing.com/profile/VivianeWei_BuschWallace/cv"
+              target="_blank"
+            >
+              <i class="fab fa-xing-square"></i>
+            </a>
+          </div>
+          <div class="icon">
+            <a href="https://instagram.com/vivianewallaceart" target="_blank">
+              <i class="fab fa-instagram-square"></i>
+            </a>
+          </div>
+          <div class="icon">
+            <a
+              href="https://www.facebook.com/vivianebuschwallace"
+              target="_blank"
+            >
+              <i class="fab fa-facebook-square"></i>
+            </a>
+          </div>
+        </div>
+      </div>
       <form>
         <label for="firstname">First Name</label>
-        <input type="text" id="firstname" placeholder="Your name.." />
+        <input type="text" id="firstname" placeholder="Your first name" />
 
         <label for="lastname">Last Name</label>
-        <input type="text" id="lastname" placeholder="Your last name.." />
+        <input type="text" id="lastname" placeholder="Your last name" />
+
+        <label for="email">Last Name</label>
+        <input type="email" id="email" placeholder="Your e-mail address" />
 
         <textarea placeholder="Write me..."></textarea>
 
