212861de532604fdc5213cdf0cd08cd0f70ee023
viviane <v.buschwallace@yahoo.com>
2021-06-16 08:52:25 +0200

    added form in contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,19 @@
 import React from "react";
 const Contact = () => {
-  return <div className="contact-wrapper">CONTACT</div>;
+  return (
+    <div className="contact-wrapper">
+      <form>
+        <label for="firstname">First Name</label>
+        <input type="text" id="firstname" placeholder="Your name.." />
+
+        <label for="lastname">Last Name</label>
+        <input type="text" id="lastname" placeholder="Your last name.." />
+
+        <textarea placeholder="Write me..."></textarea>
+
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 };
 export default Contact;
