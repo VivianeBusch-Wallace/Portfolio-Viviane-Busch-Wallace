2a1183940e7853f3e72df62dea54ff549382e386
viviane <v.buschwallace@yahoo.com>
2021-06-17 10:08:36 +0200

    added empty styling files: abstracts, components, mixins, variables, navigation

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,9 @@ const Home = () => {
     <div className="home-wrapper">
       <header className="hero">
         <div>
-          <h1>Hey, you found me!</h1>
+          <h1>Hey, you found me! Welcome!</h1>
+          <h2>My name is Viviane Wei Busch-Wallace.</h2>
+          <h2>I'm a junior fullstack web developer...</h2>
         </div>
       </header>
     </div>
