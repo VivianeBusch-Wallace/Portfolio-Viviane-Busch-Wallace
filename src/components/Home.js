7f5f5f82eb87a2079a41ead2f8150f4683c1e4cb
viviane <v.buschwallace@yahoo.com>
2021-06-17 10:39:58 +0200

    imported styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ const Home = () => {
   return (
     <div className="home-wrapper">
       <header className="hero">
-        <div>
+        <div className="hero-text">
           <h1>Hey, you found me! Welcome!</h1>
           <h2>My name is Viviane Wei Busch-Wallace.</h2>
           <h2>I'm a junior fullstack web developer...</h2>
