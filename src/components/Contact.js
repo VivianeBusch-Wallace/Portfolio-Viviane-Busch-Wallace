c9d51cfb79394ab29481fe6c274895ba63248fc6
viviane <v.buschwallace@yahoo.com>
2021-06-20 16:14:52 +0200

    added wrapper to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -23,62 +23,71 @@ const Contact = () => {
         {
           // social media contact with links
         }
-        <div className="social-media">
-          <h3>
-            Give me a holler! <FontAwesomeIcon icon={["far", "bell"]} />
-          </h3>
-          <div class="social-media icon-wrapper">
-            <div class="icon">
-              <a href="https://github.com/VivianeBusch-Wallace">
-                <FontAwesomeIcon icon={["fab", "github-square"]} />
-              </a>
-            </div>
-            <div class="icon">
-              <a
-                href="https://www.linkedin.com/in/viviane-busch-wallace-68a117179/"
-                target="_blank"
-              >
-                <FontAwesomeIcon icon={["fab", "linkedin"]} />
-              </a>
-            </div>
-            <div class="icon">
-              <a
-                href="https://www.xing.com/profile/VivianeWei_BuschWallace/cv"
-                target="_blank"
-              >
-                <FontAwesomeIcon icon={["fab", "xing-square"]} />
-              </a>
-            </div>
-            <div class="icon">
-              <a href="https://instagram.com/vivianewallaceart" target="_blank">
-                <FontAwesomeIcon icon={["fab", "instagram-square"]} />
-              </a>
+        <h2>
+          Give me a holler! <FontAwesomeIcon icon={["far", "bell"]} />
+        </h2>
+        <div className="social-media-form-wrapper">
+          <div className="social-media">
+            <div class="social-media icon-wrapper">
+              <div class="icon">
+                <a href="https://github.com/VivianeBusch-Wallace">
+                  <FontAwesomeIcon icon={["fab", "github-square"]} />
+                </a>
+              </div>
+              <div class="icon">
+                <a
+                  href="https://www.linkedin.com/in/viviane-busch-wallace-68a117179/"
+                  target="_blank"
+                >
+                  <FontAwesomeIcon icon={["fab", "linkedin"]} />
+                </a>
+              </div>
+              <div class="icon">
+                <a
+                  href="https://www.xing.com/profile/VivianeWei_BuschWallace/cv"
+                  target="_blank"
+                >
+                  <FontAwesomeIcon icon={["fab", "xing-square"]} />
+                </a>
+              </div>
+              <div class="icon">
+                <a
+                  href="https://instagram.com/vivianewallaceart"
+                  target="_blank"
+                >
+                  <FontAwesomeIcon icon={["fab", "instagram-square"]} />
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-        {
-          // contact form
-        }
-        <form>
-          <div className="fn-wrapper">
-            <label for="firstname">First Name</label>
-            <input type="text" id="firstname" placeholder="Your first name" />
-          </div>
-          <div className="ln-wrapper">
-            <label for="lastname">Last Name</label>
-            <input type="text" id="lastname" placeholder="Your last name" />
-          </div>
-          <div className="email-wrapper">
-            <label for="email">Last Name</label>
-            <input type="email" id="email" placeholder="Your e-mail address" />
-          </div>
-          <textarea placeholder="Write me..."></textarea>
+          {
+            // contact form
+          }
+          <form>
+            <div className="fn-wrapper">
+              <label for="firstname">First Name</label>
+              <input type="text" id="firstname" placeholder="Your first name" />
+            </div>
+            <div className="ln-wrapper">
+              <label for="lastname">Last Name</label>
+              <input type="text" id="lastname" placeholder="Your last name" />
+            </div>
+            <div className="email-wrapper">
+              <label for="email">Last Name</label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Your e-mail address"
+              />
+            </div>
+            <textarea placeholder="Write me..."></textarea>
 
-          <FontAwesomeIcon icon={["far", "paper-plane"]} />
-          <button type="button" className="submit-btn">
-            Submit
-          </button>
-        </form>
+            <FontAwesomeIcon icon={["far", "paper-plane"]} />
+            <button type="submit" className="submit-btn">
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
